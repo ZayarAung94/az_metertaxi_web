dd946df4f13968f486fa42254577bbd7dc07aafb
ZayarAung94 <nyanhtetshine.az@gmail.com>
2026-06-13 22:54:48 +0630

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://zayaraung94.github.io",
+  base: "/az_metertaxi_web/", // သင့် repository နာမည်ကို အတိအကျထည့်ပါ
+  output: "static",
+});
